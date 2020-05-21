d6cffa9fbb9c45949d500988aac6d9dc9e93719c
Glitch (my-issue-tracker-fcc) <none>
2020-05-21 20:33:54 +0000

    🚂💍 Updated with Glitch

--- routes/api.js
+++ routes/api.js
@@ -1,51 +1,87 @@
-/*
- *
- *
- *       Complete the API routing below
- *
- *
- */
-
 "use strict";
 
-const expect = require("chai").expect;
-const mongoose = require("mongoose");
+const Issue = require("../models/issueModel"),
+  mongoose = require("mongoose"),
+  { expect } = require("chai");
 
 // prev
 let issues = [];
 const shortid = require("shortid");
 
-//   const MongoClient = require("mongodb"),
-//   ObjectId = require("mongodb").ObjectID,
-// // const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
-
-// pre
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-const IssueSchema = new mongoose.Schema({
-  issueTitle: String,
-  issueText: String,
-  createdBy: String,
-  assignedTo: String,
-  statusText: String
-});
-
-const Issue = mongoose.model("Issue", IssueSchema);
-
-module.exports = function(app) {
+module.exports = app => {
   app
     .route("/api/issues/:project")
 
-    .get(function(req, res) {
-    
-      // prev
-      var project = req.params.project;
+    .get((req, res) => {
+      const { project } = req.params;
+
+      const issues = Issue.find(
+        { project },
+        { project: false, _v: false },
+        (err, docs) => {
+          if (err)
+            res.status(404).json(`No issues could be found for this project.`);
+          res.status(200).json(docs);
+        }
+      );
+
+      //       // prev
+      //       let {
+      //         _id,
+      //         issue_title,
+      //         issue_text,
+      //         created_by,
+      //         assigned_to,
+      //         status_text,
+      //         open
+      //       } = req.query;
+
+      //       const sentIssues = [...issues];
+
+      //       if (_id) {
+      //         sentIssues = sentIssues.filter(issue => issue._id === _id);
+      //       }
+
+      //       if (issue_title) {
+      //         sentIssues = sentIssues.filter(
+      //           issue => issue.issue_title === issue_title
+      //         );
+      //       }
+
+      //       if (issue_text) {
+      //         sentIssues = sentIssues.filter(
+      //           issue => issue.issue_text === issue_text
+      //         );
+      //       }
+
+      //       if (created_by) {
+      //         sentIssues = sentIssues.filter(
+      //           issue => issue.created_by === created_by
+      //         );
+      //       }
+
+      //       if (assigned_to) {
+      //         sentIssues = sentIssues.filter(
+      //           issue => issue.assigned_to === assigned_to
+      //         );
+      //       }
+
+      //       if (status_text) {
+      //         sentIssues = sentIssues.filter(
+      //           issue => issue.status_text === status_text
+      //         );
+      //       }
+
+      //       if (open) {
+      //         open = open === "false" ? false : true;
+      //         sentIssues = sentIssues.filter(issue => issue.open === open);
+      //       }
+
+      //       return res.json(sentIssues);
     })
 
-    .post(function(req, res) {
+    .post((req, res) => {
+      const { project } = req.params;
       const {
         issue_title,
         issue_text,
@@ -54,53 +90,67 @@ module.exports = function(app) {
         status_text
       } = req.body;
 
-      const newIssue = {
+      const issue = new Issue({
+        project,
         issue_title,
         issue_text,
+        created_on: Date.now(),
+        updated_on: Date.now(),
         created_by,
         assigned_to,
-        status_text,
-        created_on: new Date(),
-        updated_on: new Date(),
         open: true,
-        _id: shortid.generate()
-      };
-
-      issues.push(newIssue);
-
-      res.json(newIssue);
-      // prev
-      const { project } = req.params;
-      // or change it in the HTML file?
-      const {
-        issue_title: issueTitle,
-        issue_text: issueText,
-        created_by: createdBy,
-        assigned_to: assignedTo,
-        status_text: statusText
-      } = req.body;
-
-      const issue = new Issue({
-        issueTitle,
-        issueText,
-        createdBy,
-        assignedTo,
-        statusText
+        status_text
       });
 
       issue
         .save()
-        .then(d => console.log(`The issue ${d.issueTitle} was added.`))
+        // .then(d => console.log(`The issue ${d.issueTitle} was added.`))
+        .then(response => {
+          const data = {
+            _id: response._id,
+            issue_title: response.issue_title,
+            issue_text: response.issue_text,
+            created_on: response.created_on,
+            updated_on: response.updated_on,
+            created_by: response.created_by,
+            assigned_to: response.assigned_to,
+            open: response.open,
+            status_text: response.status_text
+          };
+
+          return res.status(200).json(data);
+        })
         .catch(err => console.log("there was an error: ", err));
+
+      // prev
+      //       const {
+      //         issue_title,
+      //         issue_text,
+      //         created_by,
+      //         assigned_to,
+      //         status_text
+      //       } = req.body;
+
+      //       const newIssue = {
+      //         issue_title,
+      //         issue_text,
+      //         created_by,
+      //         assigned_to,
+      //         status_text,
+      //         created_on: new Date(),
+      //         updated_on: new Date(),
+      //         open: true,
+      //         _id: shortid.generate()
+      //       };
+
+      //       issues.push(newIssue);
+
+      //       res.json(newIssue);
     })
 
-    .put(function(req, res) {
-      var project = req.params.project;
-
-      let updated = false;
-
+    .put((req, res) => {
       const {
-        _id,
+        _id: id,
         issue_title,
         issue_text,
         created_by,
@@ -109,66 +159,70 @@ module.exports = function(app) {
         open
       } = req.body;
 
-      if (
-        !issue_title &&
-        !issue_text &&
-        !created_by &&
-        !assigned_to &&
-        !status_text &&
-        !open
-      ) {
-        return res.json({ error: "no updated field sent" });
-      }
+      if (!id) res.status(400).send("_id error");
 
-      // map over the issues and change where _id from the issue is === _id provided
-      issues = issues.map(issue => {
-        if (issue._id === _id) {
-          const { created_on } = issue;
+            if (
+              !issue_title &&
+              !issue_text &&
+              !created_by &&
+              !assigned_to &&
+              !status_text &&
+              !open
+            ) {
+              return res.status(400).send("no updated field sent");
+            }
 
-          updated = true;
+      const issue = new Issue(req.body);
+      issue.updated_on = Date.now();
 
-          return {
-            _id,
-            created_on,
-            updated_on: new Date(),
-            created_by: created_by !== "" ? created_by : issue.created_by,
-            issue_title: issue_title !== "" ? issue_title : issue.issue_title,
-            issue_text: issue_text !== "" ? issue_text : issue.issue_text,
-            assigned_to: assigned_to !== "" ? assigned_to : issue.assigned_to,
-            status_text: status_text !== "" ? status_text : issue.status_text,
-            open: open === undefined ? false : true
-          };
-        }
-        return issue;
+      Issue.findByIdAndUpdate(id, issue, { new: true }, (err, doc) => {
+        if (err) res.status(404).send(`could not update ${id}`);
+        res.status(200).send("successfully updated");
       });
 
-      if (!updated) {
-        return res.json({ error: `Could not update ${_id}` });
-      }
+      //       let updated = false;
 
-      res.json({ success: "successfully updated" });
+      //       // map over the issues and change where _id from the issue is === _id provided
+      //       issues = issues.map(issue => {
+      //         if (issue._id === _id) {
+      //           const { created_on } = issue;
+
+      //           updated = true;
+
+      //           return {
+      //             _id,
+      //             created_on,
+      //             updated_on: new Date(),
+      //             created_by: created_by !== "" ? created_by : issue.created_by,
+      //             issue_title: issue_title !== "" ? issue_title : issue.issue_title,
+      //             issue_text: issue_text !== "" ? issue_text : issue.issue_text,
+      //             assigned_to: assigned_to !== "" ? assigned_to : issue.assigned_to,
+      //             status_text: status_text !== "" ? status_text : issue.status_text,
+      //             open: open === undefined ? false : true
+      //           };
+      //         }
+      //         return issue;
+      //       });
+
+      //       if (!updated) {
+      //         return res.json({ error: `Could not update ${_id}` });
+      //       }
+
+      //       res.json({ success: "successfully updated" });
     })
 
-    .delete(function(req, res) {
-      var project = req.params.project;
+    // delete an issue
+    .delete((req, res) => {
+      const { project } = req.params;
+      const { _id: id } = req.body;
 
-      const { _id } = req.body;
+      if (!id) return res.status(400).send("_id error");
 
-      if (!_id) {
-        return res.json({ error: "no id provided" });
-      }
-
-      const lenBefore = issues.length;
-
-      issues.filter(issue => issue._id !== _id);
-
-      const lenAfter = issues.length;
-
-      // successfully deleted
-      if (lenBefore !== lenAfter) {
-        res.json({ success: `deleted id: ${_id}` });
-      } else {
-        return res.json({ error: `could not delete ${_id}` });
-      }
+      Issue.findByIdAndDelete(id, (err, doc) => {
+        if (err) res.status(500).send(`could not delete ${id}`);
+        res.status(200).send(`deleted ${id}`);
+      });
     });
 };
+
+// compare index.html
--- server.js
+++ server.js
@@ -1,61 +1,56 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { expect } = require('chai');
-const cors = require('cors');
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  { expect } = require("chai"),
+  cors = require("cors"),
+  app = express();
 
-const apiRoutes = require('./routes/api.js');
-const fccTestingRoutes = require('./routes/fcctesting.js');
-const runner = require('./test-runner');
+const apiRoutes = require("./routes/api.js"),
+  fccTestingRoutes = require("./routes/fcctesting.js"),
+  runner = require("./test-runner");
 
-const app = express();
-
-// prevent xss
-const helmet = require('helmet');
+// user story 1
+const helmet = require("helmet");
 app.use(helmet());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.use(cors({ origin: '*' })); // For FCC testing purposes only
+app.use("/public", express.static(`${process.cwd()}/public`));
+app.use(cors({ origin: "*" })); // For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Sample front-end
-app.route('/:project/').get(function(req, res) {
-  res.sendFile(`${process.cwd()}/views/issue.html`);
-});
+app
+  .route("/:project/")
+  .get((req, res) => res.sendFile(`${process.cwd()}/views/issue.html`));
 
 // Index page (static HTML)
-app.route('/').get(function(req, res) {
-  res.sendFile(`${process.cwd()}/views/index.html`);
-});
+app
+  .route("/")
+  .get((req, res) => res.sendFile(`${process.cwd()}/views/index.html`));
 
 // For FCC testing purposes
 fccTestingRoutes(app);
-
 // Routing for API
 apiRoutes(app);
 
 // 404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use((req, res, next) =>
   res
     .status(404)
-    .type('text')
-    .send('Not Found');
-});
+    .type("text")
+    .send("Not Found")
+);
 
 // Start our server and tests!
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port ${process.env.PORT}`);
-  if (process.env.NODE_ENV === 'test') {
-    console.log('Running Tests...');
-    setTimeout(function() {
+  if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
+    setTimeout(() => {
       try {
         runner.run();
       } catch (e) {
-        const error = e;
-        console.log('Tests are not valid:');
-        console.log(error);
+        console.log("Tests are not valid: ", e);
       }
     }, 3500);
   }
